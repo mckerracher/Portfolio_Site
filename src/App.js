ab268fb18247d621788aaba175954cd59fe18aba
mckerracher <mckerracher@gmail.com>
2021-05-09 10:17:02 -0600

    Commit

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import { Route, Switch, NavLink } from "react-router-dom";
 import About from './About';
 import Experience from './Experience';
 import Projects from './Projects';
-import Resume from './Resume';
 import Home from './Home';
 import HSC from './HSC';
 import "./App.css";
@@ -14,7 +13,7 @@ class App extends Component {
     return (
       <div className='App'>
         <div class="navbar">
-          <Navbar collapseOnSelect expand="lg">
+          <Navbar className="justify-content-end border-bottom" style={{ width: "110%" }} sticky="top" expand="lg">
               <Navbar.Brand id="name" href="/">Joshua McKerracher</Navbar.Brand>
               <Navbar.Toggle aria-controls="responsive-navbar-nav" />
               <Navbar.Collapse id="responsive-navbar-nav">
@@ -22,18 +21,16 @@ class App extends Component {
 
                       <NavDropdown title="Projects" id="collapsible-nav-dropdown">
                           <NavDropdown.Item href="/Projects">Projects Home</NavDropdown.Item>
-                          <NavDropdown.Item href="#">Hashmap Spell Checker</NavDropdown.Item>
+                          <NavDropdown.Item href="/HSC">Hashmap Spell Checker</NavDropdown.Item>
                           <NavDropdown.Item href="#">Doubly Linked List</NavDropdown.Item>
                           <NavDropdown.Item href="#">OPaL</NavDropdown.Item>
                           <NavDropdown.Item href="#">The Backlog Website</NavDropdown.Item>
-                          <NavDropdown.Item href="#">Magic Square</NavDropdown.Item>
                       </NavDropdown>
-
                       <Nav.Link href='/about'>About</Nav.Link>
-                      <Nav.Link href="/Experience">Experience</Nav.Link>
+                      <Nav.Link href="/Experience">Contact</Nav.Link>
                   </Nav>
                   <Nav>
-                      <Nav.Link href="/Resume">Resume</Nav.Link>
+                      <Nav.Link href="https://www.linkedin.com/in/joshua-mckerracher/">LinkedIn</Nav.Link>
                       <Nav.Link href="https://github.com/mckerracher">Github</Nav.Link>
                   </Nav>
               </Navbar.Collapse>
@@ -50,7 +47,6 @@ class App extends Component {
           <Route exact path='/' component={Home} />
           <Route exact path='/About' component={About} />
           <Route exact path='/Experience' component={Experience} />
-          <Route exact path='/Resume' component={Resume} />
           <Route exact path='/Projects' component={Projects} />
             <Route exact path='/HSC' component={HSC} />
             {/*DLL*/}
--- src/Projects.js
+++ src/Projects.js
@@ -1,13 +1,68 @@
 import React, { Component } from "react";
+import { Card, CardDeck } from 'react-bootstrap';
 import './Projects.css';
 
 class Projects extends Component {
     render() {
         return (
             <div className='Projects'>
-                <iframe className="repl" src="https://replit.com/@JoshuaMcKerrach/HashMap-Spell-Checker?lite=true"/>
-                {/*<div className="line"></div>*/}
 
+                <div className="CardContainer">
+
+                    <CardDeck className="pt-3">
+                    <Card>
+                        <Card.Body>
+                            <Card.Title className="text-dark">Card title</Card.Title>
+                            <Card.Text className="text-dark">
+                                This is a wider card with supporting text below as a natural lead-in to
+                                additional content. This content is a little bit longer.
+                            </Card.Text>
+                        </Card.Body>
+
+                    </Card>
+
+                    <Card>
+                        <Card.Body>
+                            <Card.Title className="text-dark">Card title</Card.Title>
+                            <Card.Text className="text-dark">
+                                This card has supporting text below as a natural lead-in to additional
+                                content.{' '}
+                            </Card.Text>
+                        </Card.Body>
+
+                    </Card>
+
+                </CardDeck>
+
+                    <div></div>
+
+                    <CardDeck className="pt-3">
+
+                        <Card>
+                            <Card.Body>
+                                <Card.Title className="text-dark">Card title</Card.Title>
+                                <Card.Text className="text-dark">
+                                    This is a wider card with supporting text below as a natural lead-in to
+                                    additional content. This content is a little bit longer.
+                                </Card.Text>
+                            </Card.Body>
+
+                        </Card>
+
+                        <Card>
+                            <Card.Body>
+                                <Card.Title className="text-dark">Card title</Card.Title>
+                                <Card.Text className="text-dark">
+                                    This card has supporting text below as a natural lead-in to additional
+                                    content.{' '}
+                                </Card.Text>
+                            </Card.Body>
+
+                        </Card>
+
+                    </CardDeck>
+
+                </div>
             </div>
         );
     }
