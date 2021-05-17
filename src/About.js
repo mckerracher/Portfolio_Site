3de9d10e6d92f973cd2639599d875834208e524e
mckerracher <mckerracher@gmail.com>
2021-05-17 14:57:40 -0600

    Commit

--- src/About.js
+++ src/About.js
@@ -54,20 +54,29 @@ class About extends Component {
                     </div>
                 </div>
 
-                <div className="LHS">
-                    <ListGroup className="list">
-                        <ListGroup.Item variant="dark">DevOps Software Engineer at Micron Technology</ListGroup.Item>
-                        <ListGroup.Item variant="secondary">B.S. Computer Science, Oregon State University, 2021</ListGroup.Item>
-                        <ListGroup.Item variant="dark">B.A. Philosophy, U.C. Santa Cruz, 2010</ListGroup.Item>
-                        {/*<ListGroup.Item variant="secondary">Interests:*/}
-                        {/*    <ul>*/}
-                        {/*        <li>Web technologies</li>*/}
-                        {/*        <li>Big data / ETL</li>*/}
-                        {/*        <li>Machine learning</li>*/}
-                        {/*        <li>AI</li>*/}
-                        {/*    </ul>*/}
-                        {/*</ListGroup.Item>*/}
-                    </ListGroup>
+                {/*<div className="LHS">*/}
+                {/*    <ListGroup className="list">*/}
+                {/*        <ListGroup.Item variant="dark">DevOps Software Engineer at Micron Technology</ListGroup.Item>*/}
+                {/*        <ListGroup.Item variant="secondary">B.S. Computer Science, Oregon State University, 2021</ListGroup.Item>*/}
+                {/*        <ListGroup.Item variant="dark">B.A. Philosophy, U.C. Santa Cruz, 2010</ListGroup.Item>*/}
+                {/*        /!*<ListGroup.Item variant="secondary">Interests:*!/*/}
+                {/*        /!*    <ul>*!/*/}
+                {/*        /!*        <li>Web technologies</li>*!/*/}
+                {/*        /!*        <li>Big data / ETL</li>*!/*/}
+                {/*        /!*        <li>Machine learning</li>*!/*/}
+                {/*        /!*        <li>AI</li>*!/*/}
+                {/*        /!*    </ul>*!/*/}
+                {/*        /!*</ListGroup.Item>*!/*/}
+                {/*    </ListGroup>*/}
+                {/*</div>*/}
+
+                <div>
+                    Hello! My name is Josh and I enjoy creating things with code. My interest in software started while working on a second bachelor's degree in
+                    Electrical Engineering. Part of the Electrical Engineering curriculum included two Computer Science courses. After my first course in CS in
+                    2019, I was hooked! Since then, I've completed a second bachelor's degree in Computer Science from Oregon State University while working full time.
+
+                    Two years later and I'm now a DevOps Engineer at Micron Technology working with a wide variety of technologies. Lately the big data/ETL technologies have been
+                    grabbing my interest. I've had the opportunity to work with PySpark, Hadoop, Apache Kafka, Apache Spark, Apache Hive.
                 </div>
 
             </div>
