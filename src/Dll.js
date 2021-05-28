9f38a294952aa83d80729398739dedcee9d13537
Joshua <mckerracher@gmail.com>
2021-05-28 15:00:46 -0600

    Updates to HSC and Opal pages.

--- src/Dll.js
+++ src/Dll.js
@@ -5,7 +5,19 @@ class Dll extends Component {
     render() {
         return (
             <div className='Projects'>
-                <iframe className="repl" src="https://replit.com/@JoshuaMcKerrach/Doubly-Linked-List?lite=true"/>
+                <h1>Doubly Linked List</h1>
+                <div className="instructions">
+                <ul>
+                    <li>To see the source code:
+                        <ul>
+                            <li>Select the "files" icon in the top left portion of the replit window.
+                                Expand the "Double Linked List" and "src" folders, and then open the IUDoubleLinkedList.java file.</li>
+                            <li>Or, see it on Github <a href="https://github.com/mckerracher/Doubly-Linked-List">here</a>.</li>
+                        </ul>
+                    </li>
+                </ul>
+                </div>
+                <iframe className="repl" src="https://replit.com/@JoshuaMcKerrach/Doubly-Linked-List?lite=false"/>
             </div>
         );
     }
